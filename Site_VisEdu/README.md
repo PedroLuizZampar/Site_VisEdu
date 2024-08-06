004e95bdb67a4236238eaf1e45d8bce7a68b795c
Pedro Luiz Pereira Zampar <143359525+PedroLuizZampar@users.noreply.github.com>
2024-08-06 09:08:00 -0300

    Delete Site_VisEdu directory

--- Site_VisEdu/README.md
+++
@@ -1,1 +0,0 @@
-# Site_VisEdu
--- Site_VisEdu/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_route = Blueprint("home", __name__)
-
-@home_route.route('/')
-def home():
-    return render_template("index.html")
