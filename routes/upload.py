edf92df45eb815edd7b27160416179e382719ddd
Pedro Luiz Pereira Zampar <143359525+PedroLuizZampar@users.noreply.github.com>
2024-08-16 12:31:28 -0300

    Aplicando lógica para visualizar vídeo e não deixar subir arquivo com caracteres especiais

--- routes/upload.py
+++ routes/upload.py
@@ -1,5 +1,6 @@
 import os
-from flask import Blueprint, url_for, render_template, redirect, flash, request
+import re
+from flask import Blueprint, url_for, render_template, redirect, send_from_directory, request
 from werkzeug.utils import secure_filename
 from database.models.upload import Upload
 
@@ -32,6 +33,12 @@ def inserir_upload():
     if f and allowed_file(f.filename):
         basepath = os.path.dirname(__file__)
         data = request.form
+
+        # Substituir espaços por underlines
+        nome_arquivo = data['nome_arquivo'].replace(' ', '_')
+
+        if not re.match(r'^[a-zA-Z0-9_]+$', nome_arquivo):
+            return "O nome do arquivo só pode conter letras, números e underlines!"
         
         # Usa o nome fornecido pelo usuário no formulário
         filename = secure_filename(data['nome_arquivo'])
@@ -40,7 +47,7 @@ def inserir_upload():
         f.save(caminho_arquivo)
 
         Upload.create(
-            nome_arquivo = data['nome_arquivo'],
+            nome_arquivo = nome_arquivo,
             turma = data['turma'],
             data_registro = data['data_registro'],
             hora_registro = data['hora_registro'],
@@ -96,6 +103,23 @@ def atualizar_upload(upload_id):
         upload_editado.save() # Salva o novo registro no banco
 
         return redirect(url_for('home.home'))
+
+@upload_route.route('/<int:upload_id>/view')
+def reproduzir_video (upload_id):
+
+    """ Reproduz o vídeo """
+
+    upload = Upload.get_by_id(upload_id)
+    nome_arquivo = upload.nome_arquivo
+
+    return render_template('video.html', nome_arquivo=nome_arquivo)
+
+@upload_route.route('/uploads/<filename>')
+def uploaded_file(filename):
+
+    basepath = os.path.dirname(__file__)
+    
+    return send_from_directory(os.path.join(basepath, os.pardir, 'uploads'), filename)
 
 @upload_route.route('/<int:upload_id>/delete', methods=["DELETE"])
 def deletar_upload(upload_id):
