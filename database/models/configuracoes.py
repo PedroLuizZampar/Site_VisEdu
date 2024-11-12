dd780f2932a34d1b6dda31fae674ff5c64bf13b2
PedroLuizZampar <pedroluizzampar@gmail.com>
2024-11-12 17:05:32 -0300

    Implementação do CRUD de configurações

--- database/models/configuracoes.py
+++ database/models/configuracoes.py
@@ -2,6 +2,7 @@ from peewee import Model, IntegerField
 from database.database import db
 
 class Configuracoes(Model): # O Peewee já cria um ID automático, por isso não criamos um
+    qtde_padrao_frames_video = IntegerField()
     intervalo_frames = IntegerField()
 
     class Meta:
--- routes/configuracoes.py
+++ routes/configuracoes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, redirect, url_for, session, request
 from funcoes_extras import alterando_sessions_para_false
 from database.models.configuracoes import Configuracoes
 
@@ -18,3 +18,25 @@ def lista_configuracoes():
     configuracao = Configuracoes.get_or_none(Configuracoes.id == 1)
     
     return render_template('configuracoes/configuracoes_templates/lista_configuracoes.html', configuracao=configuracao)
+
+@config_route.route('/form_configuracoes')
+def form_configuracoes():
+    configuracao = Configuracoes.get_or_none(Configuracoes.id == 1)
+
+    return render_template('/configuracoes/configuracoes_templates/form_configuracoes.html', configuracao=configuracao)
+
+@config_route.route('configuracao-<int:configuracao_id>/update', methods=["POST"])
+def atualizar_configuracoes(configuracao_id):
+    if request.form.get('_method') == "PUT":
+        data = request.form
+
+        configuracao = Configuracoes.get_by_id(configuracao_id)
+
+        configuracao_editada = configuracao
+
+        configuracao_editada.qtde_padrao_frames_video = data['qtde_frames']
+        configuracao_editada.intervalo_frames = int(data['delay']) * int(data['qtde_frames'])
+
+        configuracao_editada.save()
+
+    return redirect(url_for('configuracoes.tela_configuracoes'))
